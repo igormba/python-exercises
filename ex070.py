b59c3b48ae153bdd60a40cbc8fdc3332331d49fe
Igor Andrade <gigormagno@gmail.com>
2020-05-22 18:15:53 -0300

    Resposta de exercicios.

--- ex070.py
+++ ex070.py
@@ -3,3 +3,25 @@ No final, mostre:
 A) Qual é o total gasto na compra.
 B) Quantos produtos custam mais de R$1000.
 C) Qual é o nome do produto mais barato.'''
+
+produto = preco = maisdemil = maisbarato = total = 0
+print('=-=' * 5)
+print('MERCADO ANDRADE')
+print('=-=' * 5)
+while True:
+    produto = str(input('Nome do Produto: '))
+    preco = float(input('Preço: R$ '))
+    total += preco
+    continuar = ' '
+    while continuar not in 'SN':
+        continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+        if preco > 1000:
+            maisdemil += 1
+        if preco < 1000:
+            maisbarato  = preco
+            maisbarato = produto
+    if continuar == 'N':
+        break
+print(f'O valor total da compra é R${total:.2f}.')
+print(f'{maisdemil} produtos tem valor superior à R$1000.')
+print(f'O produto mais barato é o {maisbarato}.')
--- ex071.py
+++ ex071.py
@@ -1,3 +1,19 @@
 '''Crie um programa que simule o funcionamento de um caixa eletrônico. No inicio, pergunte ao usuário qual será o valor a
 ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor será entregue.
 OBS: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.'''
+
+print('=-=' * 5)
+print('BANCO ANDRADE')
+print('=-=' * 5)
+lista = [50, 20, 10, 1]
+valor = int(input('Insira o valor que desaja sacar R$ '))
+cont = 0
+while True:
+    if valor >= lista[cont]:
+        nota = valor // lista[cont]
+        print(f'Total de {nota} notas de R$ {lista[cont]}')
+        valor = valor % lista[cont]
+    cont += 1
+    if valor == 0:
+        break
+print('Volte sempre!')
